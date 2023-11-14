d5994d0bec7d3953a362be32c88b8ffdbc0b23ad
Adolfo <ahan@experticket.com>
2023-11-14 16:18:09 +0100

    Session and advanced selector refactor

--- TicketPurchaseAutomationTest/Program.cs
+++ TicketPurchaseAutomationTest/Program.cs
@@ -2,7 +2,7 @@ namespace TicketPurchaseAutomationTest;
 
 public abstract class Program
 {
-    private static void Main(string[] args)
+    /*private static void Main(string[] args)
     {
-    }
+    }*/
 }
